7b7e8c58e64d2366f7ce3e352f64d41ef2966bb7
毛晗扬 <maohanyang789@163.com>
2018-06-25 00:46:46 +0800

    set case sensitive

---
+++ core/color.h
@@ -0,0 +1,26 @@
+#ifndef COLOR_H
+#define COLOR_H
+#include "json/json.h"
+#include <string>
+
+class Color
+{
+	private:
+		double r,g,b;
+	public:
+		Color();
+		Color(double r,double g,double b);
+		Color operator += (const Color&a);
+		void accept(const Json::Value& val);
+		double getR()const {return r;}
+		double getG()const {return g;}
+		double getB()const {return b;}
+		friend Color operator +(const Color& a,const Color &b);
+		friend Color operator *(const Color& a,double k);
+		friend Color operator *(const Color& a,const Color& b);
+		Color exp()const;
+		std::string description()const;
+		Color adjust()const;
+};
+
+#endif
